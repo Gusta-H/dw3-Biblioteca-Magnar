df6511ddb2842578eb1621b0c7cd28d60576b8c9
Raphael <raphaelmicucci@gmail.com>
2024-11-20 22:09:05 -0300

    criadas rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';  // Importando o Header
 import Home from './components/Home';      // Página Home
-import DoeLivrosForm from './components/DoeLivrosForm';  // Formulário de doação
+import Livros from './pages/Livros';
+import Livro from './pages/Livro';
+import CadastrarLivro from './pages/CadastrarLivro';
+import EditarLivro from './pages/EditarLivro';
+import Emprestimos from './pages/Emprestimos';
+import CadastrarEmprestimo from './pages/CadastrarEmprestimo';
+import Usuarios from './pages/Usuarios';
+import Usuario from './pages/Usuario';
+import CadastrarUsuario from './pages/CadastrarUsuario';
 
 function App() {
   return (
     <Router>
-      <Header />  {/* O Header será exibido em todas as páginas */}
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Página inicial com Header */}
-        <Route path="/cadastrar-doacao" element={<DoeLivrosForm />} />  {/* Formulário de doação */}
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/livros" element={<Livros />} />
+        <Route path="/livros/id" element={<Livro />} />
+        <Route path="/livros/adicionar" element={<CadastrarLivro />} />
+        <Route path="/livros/editar/id" element={<EditarLivro />} />
+        <Route path="/emprestimos" element={<Emprestimos />} />
+        <Route path="/emprestimos/criar" element={<CadastrarEmprestimo />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/usuarios/id" element={<Usuario />} />
+        <Route path="/usuarios/criar" element={<CadastrarUsuario />} />
       </Routes>
     </Router>
   );
